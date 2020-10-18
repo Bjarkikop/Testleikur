b9cfd1335f7a228c4c3ccb8cb8f9e98a31de3f0e
Bjarkikop <42611493+Bjarkikop@users.noreply.github.com>
2020-10-18 14:35:53 +0100

    test

---
+++ LeikjaForr2/Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Healthbar healthbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+        if (other.CompareTag("Enemy"))
+        {
+            
+            TakeDamage(20);
+            Debug.Log("Current Health: " + currentHealth);
+        }
+        
+    }
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthbar.SetHealth(currentHealth);
+    }
+
+}
---
+++ LeikjaForr2/Assets/bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int damage = 40;
+    void Start()
+    {
+        rb.velocity = transform.right * speed; 
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnemyHealth enemy = collision.GetComponent<EnemyHealth>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        
+    }
+
+
+}
